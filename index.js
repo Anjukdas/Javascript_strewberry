d86b9be15495ad21319b4f8b2e61ddaa2e541267
AnjuKDas <anjukollara@gmail.com>
2025-02-19 23:20:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let fruit= "strawberry";
+let newfruit ="";
+for(let i =0;i<fruit.length;i++){
+//console.log(fruit[i])
+if(fruit[i]=="r"){
+newfruit+='k'
+}else{
+newfruit+=fruit[i]
+
+}
+}
+console.log(newfruit)
